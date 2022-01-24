d52cb75ddefeb133942a09fe60b4b2fc234807e5
Станислав Афанасьев <stanislavafanasev@MacBook-Air-Stanislav.local>
2022-01-24 13:19:23 +0300

    создание компонента p

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Htag, Button } from '../components';
+import { Htag, Button, Ptag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -7,6 +7,9 @@ export default function Home(): JSX.Element {
         <Htag tag="h1">Text</Htag>
         <Button appearance="primary" arrow='right'>Узнать подробнее</Button>
         <Button appearance="ghost" arrow='down'>Узнать подробнее</Button>
+        <Ptag size='s'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cupiditate distinctio doloribus eius, eligendi laudantium porro suscipit. Accusantium autem id in incidunt minima, natus nesciunt nulla qui velit voluptatibus.</Ptag>
+        <Ptag size='m'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cupiditate distinctio doloribus eius, eligendi laudantium porro suscipit. Accusantium autem id in incidunt minima, natus nesciunt nulla qui velit voluptatibus.</Ptag>
+        <Ptag size='l'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda aut cupiditate distinctio doloribus eius, eligendi laudantium porro suscipit. Accusantium autem id in incidunt minima, natus nesciunt nulla qui velit voluptatibus.</Ptag>
       </>
   )
 }

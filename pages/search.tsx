c9f90384ea6538e4753e4be40ab265f7549fc9b1
stats <stasafanasev21220@gmail.com>
2022-02-07 14:50:52 +0300

    коммит

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,34 +1,34 @@
-import {GetStaticProps} from 'next';
+import { GetStaticProps } from 'next';
 import React from 'react';
-import {withLayout} from '../layout/Layout';
+import { withLayout } from '../layout/Layout';
 import axios from 'axios';
-import {MenuItem} from '../interfaces/menu.interface';
+import { MenuItem } from '../interfaces/menu.interface';
 
 function Search(): JSX.Element {
 
-  return (
-    <>
-      Search
-    </>
-  );
+	return (
+		<>
+			Search
+		</>
+	);
 }
 
 export default withLayout(Search);
 
-export const getStaticProps: GetStaticProps<HomeProps> = async() => {
-  const firstCategory = 0;
-  const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
-    firstCategory
-  });
-  return {
-    props: {
-      menu,
-      firstCategory
-    }
-  };
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
 };
 
 interface HomeProps extends Record<string, unknown> {
-  menu: MenuItem[];
-  firstCategory: number;
+	menu: MenuItem[];
+	firstCategory: number;
 }

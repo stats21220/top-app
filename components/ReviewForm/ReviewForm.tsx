46a183ba8323762df367f692c677af3385a72cbf
stats <stasafanasev21220@gmail.com>
2022-02-15 09:37:25 +0300

    8 sent form

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -1,4 +1,4 @@
-import React, {forwardRef} from 'react';
+import React, {useState} from 'react';
 import styles from './ReviewForm.module.css';
 import {ReviewFormProps} from './ReviewForm.props';
 import cn from 'classnames';
@@ -8,13 +8,29 @@ import {Input} from '../Input/Input';
 import {Button} from '../Button/Button';
 import CloseItem from './close.svg'
 import {useForm, Controller} from 'react-hook-form';
-import {IReviewForm} from './ReviewForm.interface';
+import {IReviewForm, IReviewSentResponse} from './ReviewForm.interface';
+import axios, {AxiosError} from 'axios';
+import {API} from '../../helpers/api';
 
 export const ReviewForm = ({productId, className, ...props}: ReviewFormProps): JSX.Element => {
-  const {register, control, handleSubmit, formState: {errors}} = useForm<IReviewForm>()
+  const {register, control, handleSubmit, formState: {errors}, reset} = useForm<IReviewForm>()
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
 
-  const onSubmit = (data: IReviewForm) => {
-    console.log(data)
+  const onSubmit = async(dataForm: IReviewForm) => {
+    try {
+      const {data} = await axios.post<IReviewSentResponse>(API.review.createDemo, {...dataForm, productId})
+      if (data.message) {
+        setIsSuccess(true)
+        reset()
+      } else {
+        setError('Что-то пошло не так')
+      }
+    } catch (e: AxiosError) {
+      if (axios.isAxiosError(e)) {
+        setError(e.message)
+      }
+    }
   }
 
   return (
@@ -79,11 +95,15 @@ export const ReviewForm = ({productId, className, ...props}: ReviewFormProps): J
           <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
         </div>
       </div>
-      <div className={styles.success}>
-        <div className={styles.successTitle}>Ваш отзыв отправлен</div>
-        <div>Спасибо, ваш отзыв будет опубликован после проверки</div>
-        <CloseItem className={styles.close}/>
-      </div>
+      {isSuccess && <div className={cn(styles.success, styles.panel)}>
+		  <div className={styles.successTitle}>Ваш отзыв отправлен</div>
+		  <div>Спасибо, ваш отзыв будет опубликован после проверки</div>
+		  <CloseItem className={styles.close} onClick={() => setIsSuccess(false)}/>
+	  </div>}
+      {error && <div className={cn(styles.error, styles.panel)}>
+		  Что-то пошло не так, попробуйте обновить страницу
+		  <CloseItem className={styles.close} onCliclk={() => setError(undefined)}/>
+	  </div>}
     </form>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import {Button, Htag, Input, P, Rating, Tag, Textarea} from '../components';
 import {withLayout} from '../layout/Layout';
 import axios from 'axios';
 import {MenuItem} from '../interfaces/menu.interface';
+import {API} from '../helpers/api';
 
 function Home({menu}: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
@@ -21,8 +22,8 @@ function Home({menu}: HomeProps): JSX.Element {
       <Tag size="s" color="green">Green</Tag>
       <Tag color="primary">Green</Tag>
       <Rating rating={rating} isEditable setRating={setRating}/>
-      <Input placeholder='test'/>
-      <Textarea placeholder='Текст отзыва'/>
+      <Input placeholder="test"/>
+      <Textarea placeholder="Текст отзыва"/>
     </>
   );
 }
@@ -31,7 +32,7 @@ export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async() => {
   const firstCategory = 0;
-  const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+  const {data: menu} = await axios.post<MenuItem[]>(API.topPage.find, {
     firstCategory
   });
   return {
